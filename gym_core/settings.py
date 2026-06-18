7b4542916a2270e0c4ead82a1cae37275de93a79
M.hamoud <m48926601@gmail.com>
2026-06-18 23:21:24 +0300

    Ready for deployment

--- gym_core/settings.py
+++ gym_core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=*g@^!9rro67rwcsd1*@rwzu=*fgr@l1m^k&ke(sm(ztzx6y2$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
